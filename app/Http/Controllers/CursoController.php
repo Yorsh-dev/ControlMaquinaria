4a225eda3fb0e0e5d70c9214134128e3c355ff4b
Yorsh-dev <75707785@continental.edu.pe>
2024-09-27 20:02:15 -0500

    Se empiezan controladores

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+        // Mostrar una lista de máquinas
+
+    public function index()
+    {
+        // Aquí iría el código para obtener todas las máquinas, por ejemplo:
+        // $maquinas = Maquina::all();
+        return view('maquinas.index'); // Llama a una vista que liste las máquinas
+    }
+    // Mostrar el formulario para crear una nueva máquina
+    public function create()
+    {
+        return view('maquinas.create'); // Llama a una vista con el formulario de creación
+    }
+     // Mostrar los detalles de una máquina específica
+     public function show($idMaquina)
+     {
+         // Aquí iría el código para obtener una máquina específica por su id
+         // $maquina = Maquina::find($id);
+         return view('maquinas.show'); // Llama a una vista que muestre los detalles de la máquina
+     }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\ComponentesController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('cursos',[CursoController::class, 'index']);
+Route::get('cursos/create',[CursoController::class, 'create']);
+Route::get('cursos/{curso}',[CursoController::class, 'show']);
